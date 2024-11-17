ee2bb6deaf9bf4a295fcd55b1d3bdcd23d129768
Dysivet-odoo <dysprosium.239@gmail.com>
2024-11-17 16:27:55 +0200

    Fix two lists of u and v

--- model.js
+++ model.js
@@ -19,23 +19,23 @@ const step_v = 2 * pi / COUNT_POINTS;
 
 function CreateSurfaceData(u_polylines, v_polylines)
 {
-    let vertexList = [];
-
+    let vertexListU = [];
     for(const u of u_polylines){
         for(const v of v_polylines){
             let vertex = getVector(u, v);
-            vertexList.push(...vertex);
+            vertexListU.push(...vertex);
         }
     }
 
+    let vertexListV = [];
     for(const v of v_polylines){
         for(const u of u_polylines){
             let vertex = getVector(u, v);
-            vertexList.push(...vertex);
+            vertexListV.push(...vertex);
         }
     }
 
-    return vertexList;
+    return {vertexListU, vertexListV};
 }
 
 function x(u){
@@ -65,22 +65,33 @@ function getPolylines(min, max, step){
 // Constructor
 function Model(name) {
     this.name = name;
-    this.iVertexBuffer = gl.createBuffer();
+    this.iVertexBufferU = gl.createBuffer();
+    this.iVertexBufferV = gl.createBuffer();
     this.count = 0;
     this.u_polylines = getPolylines(-pi, pi, step_u);
     this.v_polylines = getPolylines(0, 2*pi, step_v);
 
     this.BufferData = function() {
         let vertices = CreateSurfaceData(this.u_polylines, this.v_polylines);
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
 
-        this.count = vertices.length/3;
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBufferU);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices.vertexListU), gl.STREAM_DRAW);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBufferV);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices.vertexListV), gl.STREAM_DRAW);
+
+        this.count = vertices.vertexListU.length/3;
     }
 
     this.Draw = function() {
 
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBufferU);
+        gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
+        gl.enableVertexAttribArray(shProgram.iAttribVertex);
+        
+        gl.drawArrays(gl.LINE_STRIP, 0, this.count);
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBufferV);
         gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
         gl.enableVertexAttribArray(shProgram.iAttribVertex);
    
